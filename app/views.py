0658e26aacd14de51d68a8986dc016af1b77c166
Times4Everyone <76495035+Times4Everyone@users.noreply.github.com>
2022-02-18 12:29:23 -0600

    Update views.py

--- app/views.py
+++ app/views.py
@@ -2,22 +2,22 @@ from django.shortcuts import render
 from app import forms
 
 def indexpage(request):
-    return render(request,'index.html')
+    return render(request,'templates/index.html')
 
 def homepage(request):
-    return render(request,'homepage.html')
+    return render(request,'templates/homepage.html')
 
 def websites(request):
     post_form = forms.PostOrganization()
-    return render(request, 'mywebsite.html', {
+    return render(request, 'templates/mywebsite.html', {
         "post_form": post_form,
     })
 
 def example(request):
     post_form = forms.PostOrganization()
-    return render(request, 'web.html', {
+    return render(request, 'templates/web.html', {
         "post_form": post_form,
     })
 
 def search(request):
-    return render(request, 'search.html')
+    return render(request, 'templates/search.html')
