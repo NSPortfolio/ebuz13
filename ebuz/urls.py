ba71d2aa24a580d9b5f37266cda587daa8e8a82f
Times4Everyone <76495035+Times4Everyone@users.noreply.github.com>
2022-02-18 12:26:02 -0600

    Update urls.py

--- ebuz/urls.py
+++ ebuz/urls.py
@@ -5,7 +5,7 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.indexpage),
+    path('/', views.indexpage),
     path('home/', views.homepage),
     path('mywebsites/', views.websites),
     path('amandasbakery/create/', views.example),
